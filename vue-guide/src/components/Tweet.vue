a356bb82210b159f3a4c48c48b8ec283cb8bacc1
Wataru823 <voyage0823@gmail.com>
2022-09-27 18:48:11 +0900

    feat: section45

--- vue-guide/src/components/Tweet.vue
+++ vue-guide/src/components/Tweet.vue
@@ -1,8 +1,21 @@
 <script setup lang="ts">
+const tweets = [{ id: '0', description: 'Hello, world!' }, { id: '1', description: 'this is the second tweet' }]
 </script>
 
 <template>
   <div class="container"></div>
+  <h1>Tweeter</h1>
+  <div class="form-container">
+    <input />
+    <button class="save-button">post</button>
+  </div>
+  <div class="tweet-container">
+    <ul>
+      <li v-for="tweet in tweets" :key="tweet.id" class="tweet-list">
+        <span>{{ tweet.description }}</span>
+      </li>
+    </ul>
+  </div><!-- /.tweet-container -->
 </template>
 
 <style scoped>
@@ -21,5 +34,19 @@
   width: 60%;
   margin-bottom: 12px;
   border-radius: 4px;
+}
+
+.tweet-list {
+  list-style: none;
+}
+
+button {
+  color: #fff;
+  font-weight: bold;
+  background-color: #68c9c9;
+  border-radius: 2px;
+  border: none;
+  width: 60px;
+  height: 22px;
 }
 </style>
